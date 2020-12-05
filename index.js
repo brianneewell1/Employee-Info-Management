9d41006ffb77b1ba0a533132b3c5f51f289bab43
Brianne <brianne.ewell1@gmail.com>
2020-12-05 13:20:24 -0700

    adding initial questions

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
 //NPM packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var console.table = require("console.table");
+var consoleTable = require("console.table");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Il0vecasey!",
+    database: "employee_infoDB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    runSelect();
+    console.log("You are connected");
+});
+
+function runSelect() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all Employees",
+                "View Employees by Department",
+                "View Employees by Role",
+                "Add Department",
+                "Add Employee",
+                "Add Role",
+                "Update Employee Role",
+                "Exit"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View all Employees":
+                    EmpAllSearch();
+                    break;
+
+                case "View Employees by Department":
+                    depSearch();
+                    break;
+
+                case "View Employees by Role":
+                    roleSearch();
+                    break;
+
+                case "Add Department":
+                    depAdd();
+                    break;
+
+                case "Add Employee":
+                    empAdd();
+                    break;
+                
+                case "Add Role":
+                    roleAdd();
+                    break;
+                
+                case "Update Employee Role":
+                    roleUpdate();
+                    break;
+
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
+}
